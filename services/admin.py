a948aa5d1d81b29573bee94ded922cc7b298f3a1
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-09 15:15:57 -0600

    new models in services app, new register from model inside the admin from services and show this config in the admin panel web

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import services
 
-# Register your models here.
+class admin_services(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+admin.site.register(services,admin_services)
