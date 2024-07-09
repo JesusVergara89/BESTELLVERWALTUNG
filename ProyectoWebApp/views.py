4652ef38a9c648f287550755a51d718e56778c53
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-09 14:27:56 -0600

    first commit

---
+++ ProyectoWebApp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'ProyectoWebApp/home.html')
+
+def services(request):
+    return render(request, 'ProyectoWebApp/services.html')
+
+def store(request):
+    return render(request, 'ProyectoWebApp/store.html')
+
+def blog(request):
+    return render(request, 'ProyectoWebApp/blog.html')
+
+def contact(request):
+    return render(request, 'ProyectoWebApp/contact.html')
