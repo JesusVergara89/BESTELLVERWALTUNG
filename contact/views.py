f9c99f03040f154a424871c49daf3e93db25577e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-10 11:38:37 -0600

    using redirect for sending a parameter inside the url, caching the data from form

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import Contact_form
 
 # Create your views here.
 def contact(request):
     form_contact=Contact_form()
+    if request.method=='POST':
+        form_contact=Contact_form(data=request.POST)
+        if form_contact.is_valid():
+            name=request.POST.get("name")
+            email=request.POST.get("email")
+            content=request.POST.get("content")
+            #print(name,email,content)
+            return redirect("/contact/?valid")
     return render(request, 'contact/contact.html', {'Form_contac':form_contact })
 
 
