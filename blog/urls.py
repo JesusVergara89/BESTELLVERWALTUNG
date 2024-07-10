1472174ebfd86d2c70a1f678c2b3d39ec7c35d7d
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-09 19:16:18 -0600

    filter categories by id

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('categories/<int:categories_id>/', views.categories, name='categories'),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Categories
 
 # Create your views here.
 
 def blog(request):
     post_=Post.objects.all()
     return render(request, 'blog/blog.html', {'posts': post_})
+
+def categories(request, categories_id):
+    categ_=Categories.objects.get(id=categories_id)
+    post_=Post.objects.filter(categories=categ_)
+    return render(request, 'blog/categories_blog.html', {'categories': categ_, 'posts': post_})
