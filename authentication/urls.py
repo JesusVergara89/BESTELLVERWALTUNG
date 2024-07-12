f9df614401213a9ab86953022d80c83faee908ad
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-12 17:37:37 -0600

    creating and using the form that is provide by django UserCreationForm

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import VRegister
 
 urlpatterns = [
-    path('', views.Authentication, name='Authentication'),
+    path('', VRegister.as_view(), name='Authentication'),
 ]
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-def Authentication(request):
-    return render(request, 'authentication/authentication.html')
+class VRegister(View):
+    
+    def get(self, request):
+        form=UserCreationForm()
+        return render(request, "authentication/authentication.html", {"form": form})
+
+    def post(self, request):
+       pass
