28ad29d988bf1d8dcc4c433c3e5b9880a7129315
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-14 14:01:44 -0600

    login almost ready, just i need to put mesaages about the login error for user view, user experience

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import VRegister, closeSession
+from .views import VRegister, closeSession, login_session
 
 urlpatterns = [
     path('', VRegister.as_view(), name='Authentication'),
     path('closesession', closeSession, name='closesession'),
+    path('loggear', login_session, name='loggear'),
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
 
@@ -25,3 +25,21 @@ class VRegister(View):
 def closeSession(request):
     logout(request)
     return redirect("home")
+
+def login_session(request):
+    if request.method == "POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username_ = form.cleaned_data.get("username")
+            pass_=form.cleaned_data.get("password")
+            user_ = authenticate(username=username_, password=pass_)
+            if user_ is not None:
+                login(request,user_)
+                return redirect("home")
+            else:
+                messages.error(request, "not a valid user")
+        else:
+            messages.error(request, "Incorrect information")
+            
+    form=AuthenticationForm()
+    return render(request, "login/login.html", {"form": form})
